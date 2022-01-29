8336350d5e1a57b9a41a54df7d9b5bcb30cc6fcb
Giovanni Silva <no-reply@gsilva.pro>
2022-01-28 21:45:24 -0300

    parte 2

--- src/main/java/pro/gsilva/catalogo/config/SecurityConfig.java
+++ src/main/java/pro/gsilva/catalogo/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -29,8 +31,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         auth.inMemoryAuthentication()
-                .withUser("giovanni").password("1234").roles("ADMIN");
+                .withUser("giovanni")
+                .password(encoder.encode("1234"))
+                .roles("ADMIN");
     }
 
     @Override
--- src/main/java/pro/gsilva/catalogo/controller/CatalogoController.java
+++ src/main/java/pro/gsilva/catalogo/controller/CatalogoController.java
@@ -1,21 +1,30 @@
 package pro.gsilva.catalogo.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import pro.gsilva.catalogo.model.Musica;
 import pro.gsilva.catalogo.service.CatalogoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CatalogoController {
     
     @Autowired 
-    CatalogoService catalogoService;
+    private CatalogoService catalogoService;
 
     @RequestMapping(value="/musicas", method=RequestMethod.GET)
     public ModelAndView getMusicas() {
@@ -23,6 +32,38 @@ public class CatalogoController {
         List<Musica> musicas = catalogoService.findAll();
         mv.addObject("musicas", musicas);
         return mv;
+    }
+
+    @RequestMapping(value="/musicas/{id}", method=RequestMethod.GET)
+    public ModelAndView getMusicaDetalhes(@PathVariable("id") long id) {
+        ModelAndView mv = new ModelAndView("musicaDetalhes");
+        Musica musica = catalogoService.findById(id);
+        mv.addObject("musica", musica);
+        return mv;
+    }
+
+    @RequestMapping(value="/addMusica", method=RequestMethod.GET)
+    public String getMusicaForm(Musica musica) {
+        return "musicaForm";
+    }
+    
+    @RequestMapping(value="/addMusica", method=RequestMethod.POST)
+    public ModelAndView salvarMusica(@Valid @ModelAttribute("musica") Musica musica, 
+           BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            ModelAndView musicaForm = new ModelAndView("musicaForm");
+            musicaForm.addObject("mensagem", "Verifique os errors do formulário");
+            return musicaForm;
+        }
+        musica.setData(LocalDate.now());
+        catalogoService.save(musica);
+        return new ModelAndView("redirect:/musicas");
+    }    
+    
+    @RequestMapping(value="/delMusica/{id}", method=RequestMethod.GET)
+    public String delMusica(@PathVariable("id") long id) {
+        catalogoService.excluir(id);
+        return "redirect:/musicas";
     }
         
 
