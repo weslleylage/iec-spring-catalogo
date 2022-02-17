81511b4f2d84078af629b23bc34e218df1ec30d3
Giovanni Silva <no-reply@gsilva.pro>
2022-02-17 14:14:28 -0300

    exemplo de custom repository

--- src/main/java/pro/gsilva/catalogo/repository/CatalogoRepository.java
+++ src/main/java/pro/gsilva/catalogo/repository/CatalogoRepository.java
@@ -7,7 +7,13 @@ import pro.gsilva.catalogo.model.Musica;
 
 import java.util.List;
 
-public interface CatalogoRepository extends JpaRepository<Musica, Long>  {
+public interface CatalogoRepository extends JpaRepository<Musica, Long>, CustomCatalogoRepository {
     List<Musica> findAllByTitulo(String titulo);
     List<Musica> findAllByTituloIsLike(String titulo);
+
+    @Query("select m from Musica m where m.titulo like :titulo")
+    List<Musica> findAllWithTituloLike(String titulo);
+
+//    @Query("select m from Musica m join m.categoria c where c.id = :categoriaId")
+//    List<Musica> findAllByCategoriaId(Long categoriaId);
 }
